fc12ad48278c5ad0095477cad8ad21c473dcf2cd
Savarese <phpsavarese@gmail.com>
2016-07-11 20:23:02 -0300

    Added polynomial feature expansion to load, and also tried standardization on LogReg.

--- load.py
+++ load.py
@@ -1,5 +1,13 @@
 import cPickle
 import numpy as np
+from sklearn import preprocessing
+from sklearn.preprocessing import PolynomialFeatures
+
+def standardize(X):
+    return preprocessing.StandardScaler().fit_transform(X)
+
+def polyExpand(X, order):
+    return PolynomialFeatures(order).fit_transform(X)
 
 def one_hot(x,n):
     if type(x) == list:
@@ -9,15 +17,19 @@ def one_hot(x,n):
     o_h[np.arange(len(x)),x] = 1
     return o_h
 
-def loadData(onehot = True):
+def loadData(onehot = True, stdize = False, poly = False):
     with open("data.pkl","rb") as f:
         data = cPickle.load(f)
     X = data["data"]
     Y = data["labels"]
-    if onehot:
-        Y = one_hot(Y,2)
+    
+    if stdize: X = standardize(X)
+    if poly != False: X = polyExpand(X, poly)
+    if onehot: Y = one_hot(Y,2)
+    
     nPoints = len(Y)
     valSplit = int(0.8*nPoints)
+    
     trX = X[:valSplit]
     teX = X[valSplit:]
     trY = Y[:valSplit]
--- logreg.py
+++ logreg.py
@@ -2,9 +2,9 @@ import load
 import numpy as np
 from sklearn import linear_model
 
-trX, teX, trY, teY = load.loadData(onehot = False)
+trX, teX, trY, teY = load.loadData(onehot = False, stdize = True)
 
-logreg = linear_model.LogisticRegression(C=1e5)
+logreg = linear_model.LogisticRegression(C=1e50)
 logreg.fit(trX, trY)
 print "Training acc: ", logreg.score(trX, trY)
 print "Test acc: ", logreg.score(teX, teY)
